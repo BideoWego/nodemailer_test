3909e2ad266181dd111d38f348e778e84caa7ce1
Bideo Wego <bideowego@gmail.com>
2017-05-10 23:24:40 -0400

    Initial commit

---
+++ services/email.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer');
+
+
+// Note this only works if less secure apps are
+// enabled with Google
+const _transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+
+const EmailService = {};
+
+
+EmailService.send = (options) => {
+  return new Promise((resolve, reject) => {
+    _transporter.sendMail(options, (err, info) => {
+      err ? reject(err) : resolve(info);
+    });
+  });
+};
+
+
+
+
+module.exports = EmailService;
+
+
+
+
+
+
+
+
+
+
