bfc4b6901cccdfa39d84efe1287b7374c2a6e4d5
Bideo Wego <bideowego@gmail.com>
2017-05-16 00:19:42 -0400

    Add separate dev transport

--- services/email.js
+++ services/email.js
@@ -4,19 +4,28 @@ const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
 
 // Use Heroku's SendGrid ENV variables
-const _options = {
-  service: 'SendGrid',
-  auth: {
-    api_user: process.env.SENDGRID_USERNAME,
-    api_key: process.env.SENDGRID_PASSWORD
-  }
-};
+// or dev transport options
+let _options;
+if (process.env.NODE_ENV === 'production') {
+  _options = sendGridTransport({
+    service: 'SendGrid',
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  });
+} else {
+  _options = {
+    service: 'gmail',
+    auth: {
+     user: process.env.EMAIL_USER,
+     pass: process.env.EMAIL_PASS
+    }
+  };
+}
 
 // Create the SendGrid transport
-const _transporter = nodemailer
-  .createTransport(
-    sendGridTransport(_options)
-  );
+const _transporter = nodemailer.createTransport(_options);
 
 
 const EmailService = {};
